e7c29860abfda92451fe28c80960f21d2dc0ce9e
cairo <cairoeth@protonmail.com>
2022-07-20 16:52:02 +0200

    Adding LINK and Oracle mocks for testing

--- test/protection.t.sol
+++ test/protection.t.sol
@@ -1,12 +1,93 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.13;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.15;
 
-import "forge-std/Test.sol";
+import {console} from "forge-std/console.sol";
+import {stdStorage, StdStorage, Test} from "forge-std/Test.sol";
 
-contract ContractTest is Test {
-    function setUp() public {}
+import {Utils} from "./utils/Utils.sol";
+import {Protection} from "../src/protection.sol";
 
-    function testExample() public {
-        assertTrue(true);
+/**
+ * @title Gradient Protection (v0.1) unit tests
+ * @author @cairoeth
+ **/
+contract BaseSetup is Protection, Test {
+    Utils internal utils;
+    address payable[] internal users;
+
+    address internal gradient;
+    address internal lender;
+
+    Protection protection_contract;
+
+    function setUp() public virtual {
+        utils = new Utils();
+        users = utils.createUsers(2);
+
+        gradient = users[0];
+        vm.label(gradient, "Gradient");
+        lender = users[1];
+        vm.label(lender, "Lender");
+
+        vm.prank(gradient);
+        protection_contract = new Protection();
     }
+}
+
+contract NFTfiAddress is BaseSetup {
+    address internal nftfi_address = 0x33e75763F3705252775C5AEEd92E5B4987622f44;
+
+    function setUp() public virtual override {
+        BaseSetup.setUp();
+    }
+
+    function testNFTfiAddress() public {
+        console.log(
+            "Should establish the NFTfi address using the owner address"
+        );
+
+        /// Ensure tx sender is owner
+        vm.prank(gradient);
+        protection_contract.setNFTfiAddress(nftfi_address);
+    }
+
+    function testFailNFTfiAddress() public {
+        console.log(
+            "Should not establish the NFTfi address as the sender is not owner"
+        );
+
+        /// Ensure tx sender is owner
+        vm.prank(address(0));
+        protection_contract.setNFTfiAddress(nftfi_address);
+    }
+    
+}
+
+contract ChainlinkSetup is BaseSetup {
+    address internal nftfi_address = 0x33e75763F3705252775C5AEEd92E5B4987622f44;
+
+    function setUp() public virtual override {
+        BaseSetup.setUp();
+    }
+
+    function testNFTfiAddress() public {
+        console.log(
+            "Should establish the NFTfi address using the owner address"
+        );
+
+        /// Ensure tx sender is owner
+        vm.prank(gradient);
+        protection_contract.setNFTfiAddress(nftfi_address);
+    }
+
+    function testFailNFTfiAddress() public {
+        console.log(
+            "Should not establish the NFTfi address as the sender is not owner"
+        );
+
+        /// Ensure tx sender is owner
+        vm.prank(address(0));
+        protection_contract.setNFTfiAddress(nftfi_address);
+    }
+    
 }
